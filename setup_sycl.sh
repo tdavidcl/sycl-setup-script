b52e6f55190b520036a8eaa1af51d8349071f4cc
David -- Cléris Timothée <timothee.davidcleri@gmail.com>
2022-11-20 20:29:25 +0100

    Update setup_sycl.sh

--- setup_sycl.sh
+++ setup_sycl.sh
@@ -89,8 +89,41 @@ then
     fi
 
     cd build
-    ninja all
-    ninja install
+    ninja all\
+        lib/libsycl-cmath-fp64.o               \
+        lib/libsycl-fallback-cstring.spv\
+        lib/libsycl-cmath.o                    \
+        lib/libsycl-fallback-imf-fp64.o\
+        lib/libsycl-complex-fp64.o             \
+        lib/libsycl-fallback-imf-fp64.spv\
+        lib/libsycl-complex.o                  \
+        lib/libsycl-fallback-imf.o\
+        lib/libsycl-crt.o                      \
+        lib/libsycl-fallback-imf.spv\
+        lib/libsycl-fallback-cassert.o         \
+        lib/libsycl-imf-fp64.o\
+        lib/libsycl-fallback-cassert.spv       \
+        lib/libsycl-imf.o\
+        lib/libsycl-fallback-cmath-fp64.o      \
+        lib/libsycl-itt-compiler-wrappers.o\
+        lib/libsycl-fallback-cmath-fp64.spv    \
+        lib/libsycl-itt-stubs.o\
+        lib/libsycl-fallback-cmath.o           \
+        lib/libsycl-itt-user-wrappers.o\
+        lib/libsycl-fallback-cmath.spv         \
+        lib/libsycl_pi_trace_collector.so\
+        lib/libsycl-fallback-complex-fp64.o    \
+        lib/libsycl_profiler_collector.so\
+        lib/libsycl-fallback-complex-fp64.spv  \
+        lib/libsycl_sanitizer_collector.so\
+        lib/libsycl-fallback-complex.o         \
+        lib/libsycl.so\
+        lib/libsycl-fallback-complex.spv       \
+        lib/libsycl.so.6\
+        lib/libsycl-fallback-cstring.o         \
+        lib/libsycl.so.6.1.0-0\
+        tools/libdevice/libsycldevice\
+        install
 
 fi
 
